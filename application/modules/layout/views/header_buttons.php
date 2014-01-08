1b79d60a37474e7289ff3251dadb9a4857306d00
Daniel Bertoni <daniel.bertoni@gmail.com>
2014-01-08 01:07:12 -0300

    bootstrap moderno

--- application/modules/layout/views/header_buttons.php
+++
@@ -1,4 +0,0 @@
-<div class="pull-right">
-	<button class="btn btn-primary" id="btn-submit" name="btn_submit" value="1"><i class="icon-ok icon-white"></i> Grabar</button>
-    <button class="btn btn-danger" id="btn-cancel" name="btn_cancel" value="1"><i class="icon-remove icon-white"></i> Cancelar</button>
-</div>
