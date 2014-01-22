5835ec8ce23d093131989e1505c56d84aaf4ffc3
Daniel Bertoni <daniel.bertoni@gmail.com>
2014-01-22 15:55:26 -0300

    inicio decaja

--- application/modules/caja/views/index.php
+++ application/modules/caja/views/index.php
@@ -9,31 +9,38 @@
         </div><!-- /.row -->
       </div><!-- /.container -->
     </div><!-- /.section-colored -->
-    
-        <div class="section-colored">
-
-      <div class="container">
-
-        <div class="row">
-          <div class="col-lg-6 col-md-6 col-sm-6">
-            <h2>Modern Business Features Include:</h2>
-            <ul>
-              <li>Bootstrap 3 Framework</li>
-              <li>Mobile Responsive Design</li>
-              <li>Predefined File Paths</li>
-              <li>Working PHP Contact Page</li>
-              <li>Minimal Custom CSS Styles</li>
-              <li>Unstyled: Add Your Own Style and Content!</li>
-              <li>Font-Awesome fonts come pre-installed!</li>
-              <li>100% <strong>Free</strong> to Use</li>
-              <li>Open Source: Use for any project, private or commercial!</li>
-            </ul>
-          </div>
-          <div class="col-lg-6 col-md-6 col-sm-6">
-            <img class="img-responsive" src="http://placehold.it/700x450/ffffff/cccccc">
-          </div>
-        </div><!-- /.row -->
-
-      </div><!-- /.container -->
-
-    </div><!-- /.section-colored -->
+<div class="container">
+    <div class="row">
+      <div class="col-lg-4 col-md-4">
+        <h3><i class="fa fa-check-circle"></i>Operciones Diarias</h3>
+        <table>
+            <tr>
+                <td><button type="button" class="btn btn-danger">Inicio de Caja</button></td>
+                <td>&nbsp;</td>
+                <td>hora de cierre</td>
+            </tr>
+            <tr><td colspan="3">&nbsp;</td></tr>
+            <tr>
+                <td><button type="button" class="btn btn-info">Planilla de Caja</button></td>
+                <td>&nbsp;</td>
+                <td>hora de cierre</td>
+            </tr>        
+            <tr><td colspan="3">&nbsp;</td></tr>
+            <tr>
+                <td><button type="button" class="btn btn-success">Cierre de Caja</button></td>
+                <td>&nbsp;</td>
+                <td>hora de cierre</td>
+            </tr>   
+        </table>                
+      </div>
+      <div class="col-lg-4 col-md-4">
+        <h3><i class="fa fa-pencil"></i>Informes</h3>
+        <p>You're ready to go with this pre-built page structure, now all you need to do is add your own custom stylings! You can see some free themes over at <a href="http://bootswatch.com">Bootswatch</a>, or come up with your own using <a href="http://getbootstrap.com/customize/">the Bootstrap customizer</a>!</p>
+      </div>
+      <div class="col-lg-4 col-md-4">
+        <h3><i class="fa fa-folder-open"></i>Ajustesy Operaciones</h3>
+        <p>This template features many common pages that you might see on a business website. Pages include: about, contact, portfolio variations, blog, pricing, FAQ, 404, services, and general multi-purpose pages.</p>
+      </div>
+    </div><!-- /.row -->
+  </div><!-- /.container -->
+</div><!-- /.section -->
